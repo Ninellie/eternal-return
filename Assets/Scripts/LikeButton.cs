f3c66e4d6b36073fa959f83876f4811ea6c379da
Ninellie <a.pawlow2009@ya.ru>
2026-08-15 11:04:01 +0300

    [Init] First commit.

---
+++ Assets/Scripts/LikeButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using VContainer;
+
+namespace EternalReturn
+{
+    public class LikeButton : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+        
+        [Inject]
+        private LikeResource _resource;
+
+        public void Like()
+        {
+            _resource.Increase();
+        }
+    }
+}
---
+++ Assets/Scripts/LikeIndicator.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using VContainer;
+
+namespace EternalReturn
+{
+    public class LikeIndicator : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private Image image;
+
+        [Inject]
+        private LikeResource _resource;
+
+        private void OnEnable()
+        {
+            UpdateIndicator(_resource.Amount);
+            _resource.OnChange += UpdateIndicator;
+        }
+
+        private void OnDisable()
+        {
+            _resource.OnChange -= UpdateIndicator;
+        }
+
+        private void UpdateIndicator(int amount)
+        {
+            text.text = amount.ToString();
+            
+            var percent = _resource.Amount / (float)_resource.MaxAmount;
+            
+            image.rectTransform.anchorMax = new Vector2(percent, 1);
+        }
+    }
+}
